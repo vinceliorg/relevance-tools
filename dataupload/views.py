971766dbef896f54361f57b87528c56d325dd000
Xianan Zhao <vli@vinceli.org>
2020-06-21 14:59:33 +1000

    view

--- dataupload/views.py
+++ dataupload/views.py
@@ -10,3 +10,8 @@ def upload_doc(request):
         if form.is_valid():
             handle_uploaded_file(request.FILES['file'])
     return render(request, 'dataupload/upload_doc.html', locals())
+
+def data_mapping(reuqest,file_path):
+    data=pd.read_csv(file_path, encoding = "ISO-8859-1")
+    return HttpResponse("The column names are  %s." % data.column.value)
+
